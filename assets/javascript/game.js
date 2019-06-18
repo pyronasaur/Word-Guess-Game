93fcf27db5a402237df8cb0511548aa8bf5a9c32
pyronasaur <pyronnicholas@gmail.com>
2019-06-17 23:54:37 -0400

    Getting keypress and limiting selection to alphabet

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -29,15 +29,39 @@ $(document).ready(function() {
         //store char array size
         charCount = choiceArray.length;
 
+        //register space to deliver blanks and letters
+        var blankSpace = document.getElementById("wordContainer");
+
         //iterate through chosen word 
         for(var i = 0; i < choiceArray.length; i++)
         {
             blanksArray.push("_ ");
-            document.write("_ ");
+            blankSpace.innerHTML += ("_ ");
+        }
+    }
+    
+    //array with acceptable key presses
+    var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    
+    //variable to store key guess
+    var keyPress;
+
+    //get key press for the document and ignore non-alphabet selection
+    document.onkeyup = function(event) {
+        //console.log(event.key.toLowerCase());
+
+        for(var i = 0; i < letters.length; i++)
+        {
+            var letter = letters[i];
+            if(event.key === letter.toLowerCase()){
+                keyPress = event.key;
+                //console.log(letters[i]);
+                //console.log(letter.toLowerCase());
+                return;
+            }
         }
     }
 
-    
 
 
 setup();
