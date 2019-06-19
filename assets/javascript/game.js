2f9bf48ba18fc107cde5fc0d6a66b46db7698b1f
pyronasaur <pyronnicholas@gmail.com>
2019-06-18 22:27:49 -0400

    working hangman letter replacement with two test words

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,6 +17,19 @@ $(document).ready(function() {
     //var to hold number of characters in the chosen hangman string array
     var charCount = 0;
 
+    //register space to deliver blanks and letters
+    var blankSpace = document.getElementById("wordContainer");
+
+    //hold number of guesses
+    var numOfGuesses;
+
+    //array with acceptable key presses
+    var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    
+    //variable to store key guess
+    var keyPress;
+
+    
     function setup(){
         //choose a word
         chosenWord = wordChoices[ Math.floor( Math.random() * wordChoices.length ) ];
@@ -27,12 +40,9 @@ $(document).ready(function() {
         console.log("chosen word split: " + choiceArray);
 
         //store char array size
-        charCount = choiceArray.length;
+        charCount = choiceArray.length;        
 
-        //register space to deliver blanks and letters
-        var blankSpace = document.getElementById("wordContainer");
-
-        //iterate through chosen word 
+        //iterate through chosen word and build blanks + empty space for player char visibility
         for(var i = 0; i < choiceArray.length; i++)
         {
             blanksArray.push("_ ");
@@ -40,12 +50,8 @@ $(document).ready(function() {
         }
     }
     
-    //array with acceptable key presses
-    var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    
-    //variable to store key guess
-    var keyPress;
 
+    
     //get key press for the document and ignore non-alphabet selection
     document.onkeyup = function(event) {
         //console.log(event.key.toLowerCase());
@@ -57,11 +63,40 @@ $(document).ready(function() {
                 keyPress = event.key;
                 //console.log(letters[i]);
                 //console.log(letter.toLowerCase());
-                return;
+
+                //capture existing keypresses and print to screen, maybe don't check them a second time
+                //blankSpace.innerHTML += keyPress;
+                break;
             }
         }
+
+        //check keyPress against choiceArray and change blanksArray[i] to letter if correct, rewrite word display
+        function play(){
+            blankSpace.innerHTML = "";
+            for(var i = 0; i < choiceArray.length; i++){
+                if(keyPress == choiceArray[i]){
+                    blanksArray[i] = keyPress + " ";
+                    blankSpace.innerHTML += blanksArray[i];
+                }
+                else{
+                    blankSpace.innerHTML += blanksArray[i];
+                }
+                
+            }
+        }
+        play();        
     }
 
+
+    //generate number of guesses and 
+    //TODO --show count on screen
+    function generateGuesses(){
+        return (Math.ceil( Math.random() * 10) + 10);
+    }
+
+
+
+    
 
 
 setup();
